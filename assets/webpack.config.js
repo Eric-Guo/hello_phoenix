dd952331404e39b7798b9894102bee088edba565
Eric-Guo <eric@managebac.com>
2018-03-31 20:55:37 +0800

    Hello Phoenix 1.4 master!

---
+++ assets/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => ({
+  entry: './js/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, '../priv/static/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader', options: { minimize: options.mode === 'production' }
+          }
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('../css/app.css'),
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+  ]
+});
---
+++ config/config.exs
@@ -0,0 +1,32 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :hello,
+  ecto_repos: [Hello.Repo]
+
+# Configures the endpoint
+config :hello, HelloWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "WNEux03wCfMZRcv+d/8q8LIhuDgAocGmSX1zPHzfnxY20hvjoyO9ErgD3p4w3Qrh",
+  render_errors: [view: HelloWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Hello.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix and Ecto
+config :phoenix, :json_library, Jason
+config :ecto, :json_library, Jason
+
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
