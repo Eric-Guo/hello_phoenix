2ee4b252ce67af414bd036b2845a44cc83bd2ad4
Eric-Guo <eric@cloud-mes.com>
2018-03-31 22:32:16 +0800

    Hello in name

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -1,7 +1,7 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
-  def world(conn, _params) do
-    render conn, "world.html"
+  def world(conn, %{"name" => name}) do
+    render conn, "world.html", name: name
   end
 end
--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -16,7 +16,7 @@ defmodule HelloWeb.Router do
   scope "/", HelloWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/hello", HelloController, :world
+    get "/hello/:name", HelloController, :world
     get "/", PageController, :index
   end
 
