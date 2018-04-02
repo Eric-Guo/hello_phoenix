2436318f42ca9270799b8d58126881345dc37b12
Eric-Guo <eric@cloud-mes.com>
2018-04-03 00:22:11 +0800

    Another page and partial view template

--- lib/hello_web/controllers/user_controller.ex
+++ lib/hello_web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule HelloWeb.UserController do
     users = Hello.Repo.all(HelloWeb.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Hello.Repo.get(HelloWeb.User, id)
+    render conn, "show.html", user: user
+  end
 end
