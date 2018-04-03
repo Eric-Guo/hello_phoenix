2c6227d2f0a1d04aa6bcd56e1b6459cc5dca328d
Eric-Guo <eric@cloud-mes.com>
2018-04-04 00:41:13 +0800

    Fix test

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "hello_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
