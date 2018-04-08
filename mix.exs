fd518aba62ec9cf3d55309ed72a1b9c3288cf59e
Eric-Guo <eric@managebac.com>
2018-04-08 16:02:26 +0800

    All versions prior to 2.2.2 could send the body out of order.

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Hello.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:cowboy, "~> 1.0"},
+      {:cowboy, "~> 2.3"},
       {:comeonin, "~> 2.0"}
     ]
   end
