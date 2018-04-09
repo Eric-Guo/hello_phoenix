6ab521dc91f61932d4022cded0918bde5a89b2ed
Eric-Guo <eric@managebac.com>
2018-04-09 22:27:17 +0800

    Add eslintrc.js

---
+++ assets/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "extends": "standard",
+  "rules": {
+      "indent": [
+          "error",
+          2
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "double"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ]
+  },
+};
